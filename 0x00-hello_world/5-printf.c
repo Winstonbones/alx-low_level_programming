40f63f07631497c91a8ae39a68eb57ed923f0253
winstonbones <remothabetsej@gmail.com>
2023-07-14 03:55:27 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,9 @@
  *
  * Return:Always 0(Success)
  */
-int main(void;/*Program with print text with warning*/
-{	/*display text*/
-	printf("with proper grammar,but the outcome is a piece of art,\n");
-	return (0);
+int main(void)
 {
+    /* Display text */
+    printf("With proper grammar, but the outcome is a piece of art,\n");
+    return 0;
+}
