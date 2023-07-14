a42d1410c0736e3d8b22d751e880f28180f88972
winstonbones <remothabetsej@gmail.com>
2023-07-13 19:11:41 -0700

    loca

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
- * return : Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-int main(void);/* program wtih print text with warning*/
-
 int main(void)
-{       /*display text*/
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-
+{
+    /* Display text */
+    printf("With proper grammar, but the outcome is a piece of art,\n");
+    return 0;
 }
