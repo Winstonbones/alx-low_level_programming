7c231e8a30111aec45172a7dbd44f90a7178b61d
winstonbones <remothabetsej@gmail.com>
2023-07-14 04:10:39 -0700

    pritn

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,6 @@
 int main(void)
 {
     /* Display text */
-    printf("With proper grammar, but the outcome is a piece of art,\n");
+    printf(" with proper grammar, but the outcome is a piece of art,\n");
     return (0);
 }
