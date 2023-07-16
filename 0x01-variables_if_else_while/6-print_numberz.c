12cd6b99257e1a2e1e87cbfe43236e365417514b
winstonbones <remothabetsej@gmail.com>
2023-07-16 08:26:31 -0700

    hasd

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	int number;
+	int num;
 
-	for (number = 0; num < 10; number++)
-		putchar((number % 10) + '0');
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
 
 	putchar('\n');
 
