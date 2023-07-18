e672c39415b9aa1a4eaa0918c9740f1f12ac44d5
winstonbones <remothabetsej@gmail.com>
2023-07-18 09:31:12 -0700

    kkk

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,18 @@
-#include "main."h
+#include "main.h"
 /**
  *main- prints 10 times the alphabet, in lowercase
  *
  *Return: Always 0
 */
-void print_alphabet_x10(void)
 
+void print_alphabet_x10(void)
 {
 	char ch
 	int i
 
 	i = 0;
 
-	while(i < 10)
+	while (i < 10)
 	{
 		char ch = ‘a’;
 		while (ch <= ‘z’)
@@ -22,7 +22,7 @@ void print_alphabet_x10(void)
 		}
 
 		_putchar(‘\n’);
-i++;
+		i++;
 	}
 }
 
