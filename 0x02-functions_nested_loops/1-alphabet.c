cc09027d1246d8055ce00ad4689daa47d017c011
winstonbones <remothabetsej@gmail.com>
2023-07-18 03:55:38 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include “main.h”
+/**
+ *main-prints the alphabet, in lowercase
+ */
+
+void print_alphabet(void)
+{
+	Char letter;
+
+	for(letter = ‘a’; letter <= ‘z’; letter++)
+			_putchar(letter);
+
+	Putchar(‘\n’);
+{
+
