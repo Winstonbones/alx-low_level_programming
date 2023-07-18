b8949b0974bdb51c414886a211f5b918717aad86
winstonbones <remothabetsej@gmail.com>
2023-07-18 10:15:49 -0700

    comit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,13 +3,14 @@
  *main-  checks for lowercase character
  *
  *Return: 0
+*/
 
 int _islower(int c)
 {
-	if (c <= ‘a’ && c <= ‘z’)
-		return(1)
+	if (c >= ‘a’ && c <= ‘z’)
+		return (1);
 	else
 
-	return (0)
+	return (0);
 }
 
