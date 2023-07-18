6b7903dedd4eb0e8f48dafd69ea532fdb0a05ba6
winstonbones <remothabetsej@gmail.com>
2023-07-18 09:55:05 -0700

    lower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *main- prints 10 times the alphabet, in lowercase
  *
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h”
+/**
+ *main-  checks for lowercase character
+ *
+ *Return: 0
+
+int _islower(int c)
+{
+	if (c <= ‘a’ && c <= ‘z’)
+		return(1)
+	else
+
+	return (0)
+}
+
