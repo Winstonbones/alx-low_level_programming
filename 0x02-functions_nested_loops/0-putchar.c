0af8ca75934435464eb1599da2dc699f50dea267
winstonbones <remothabetsej@gmail.com>
2023-07-18 18:48:55 -0700

    night

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
-/**
- *main-prints _putchar
- *
- *Return:0
-*/
 
+/**
+ * main - Print _putchar
+ * Return: Always 0
+ */
 int main(void)
 {
-_putchar(‘_‘);
-_putchar(‘p‘);
-_putchar(‘u‘);
-_putchar(‘t‘);
-_putchar(‘c‘);
-_pucthar(‘h‘);
-_putchar(‘a‘);
-_pucthar(‘r‘);
-_putchar(‘\n’);
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
-
