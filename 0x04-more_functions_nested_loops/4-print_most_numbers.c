d61af8d960c85ffc991d476507e7df467cf8bf0f
winstonbones <remothabetsej@gmail.com>
2023-07-21 02:03:05 -0700

    add

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,15 +6,15 @@
  * return: void
  */
 
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
 		if (!(c == '2' || c == '4'))
-			_putchar(c);
+			putchar(c);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
 
