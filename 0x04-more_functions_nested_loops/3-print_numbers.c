ff46185d2fd2a0ce5b4d5ef859f618460eef7b9f
winstonbones <remothabetsej@gmail.com>
2023-07-21 01:07:50 -0700

    numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
+
 /**
-*print_most_numbers - prints
+*print_numbers - print 0123456789
 *Return: void
 */
 
-void print_most_numbers(void);
+void print_numbers(void)
 {
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	if (!(c == '2' || c == '4'))
 	_putchar(c);
 	}
 	_putchar('\n');
