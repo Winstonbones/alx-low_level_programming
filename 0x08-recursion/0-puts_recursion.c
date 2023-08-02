ee687e64b685d08a69fe976ffc623d802c734eff
winstonbones <remothabetsej@gmail.com>
2023-08-02 01:29:22 -0700

    main

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _puts_recursion - a function that prints as string
  * @s: string value
- *Return: void
+ *Return: Always 0
  */
 void _puts_recursion(char *s)
 {
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,3 +9,16 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 #endif
 
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
