db79c8a823f288d6a75ba637a724d13d901fb886
winstonbones <remothabetsej@gmail.com>
2023-08-05 03:57:08 -0700

    add

---
+++ 0x0A-argc_argv/4-adds.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers.
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int k; 
+	int p; 
+	int total = 0;
+	char *checks = NULL;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (k = 1; argv[k]; k++)
+	{
+		p = strtol(argv[k], &checks, 10);
+		if (*checks)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			total += p;
+		}
+	}
+	printf("%d\n", total);
+
+	return (0);
+}
