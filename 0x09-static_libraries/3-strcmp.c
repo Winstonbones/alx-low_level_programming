c35fb683a4717ec192ff0d74d0508da234468d60
winstonbones <remothabetsej@gmail.com>
2023-08-07 12:40:38 -0700

    done

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * _strcmp - a function that compares two strings
+ * @s1: string value
+ * @s2: string value
+ *
+ * Return: s1[p] – s2[p]
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int p;
+
+	p = 0;
+	while (s1[p] != '\0' && s2[p] != '\0')
+	{
+		if (s1[p] != s2[p])
+		{
+			return (s1[p] - s2[p]);
+		}
+		p++;
+	}
+	return (0);
+}
+
---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn -  a function that gets the length of a prefix substring.
+ * @s: input value
+ * @accept: input string
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int p;
+
+	while (*s)
+	{
+		for (p = 0; accept[p]; p++)
+		{
+			if (*s == accept[p])
+			{
+				n++;
+				break;
+			}
+			else if (accept[p + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
+
