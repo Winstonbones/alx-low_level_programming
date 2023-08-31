46a7ac955a77b63d3cb173440bd5b18413dbf8c9
winstonbones <remothabetsej@gmail.co>
2023-08-31 04:32:46 -0700

    bulli

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int task;
+
+	for (i = 75; i >= 0; i--)
+	{
+		task = n << i;
+
+		if (task & 1)
+		{
+			putchar('1');
+			count++;
+		}
+		else if (count)
+			putchar('0');
+	}
+	if (!count)
+		putchar('0');
+}
+
