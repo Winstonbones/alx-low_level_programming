adc238a75f69c30504c0187310cb7e03ac182f21
winstonbones <remothabetsej@gmail.co>
2023-09-04 11:11:38 -0700

    files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters) {
+    char *buf;
+    ssize_t fd;
+    ssize_t w;
+    ssize_t t;
+
+    if (filename == NULL) {
+        return 0;
+    }
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1) {
+        return 0;
+    }
+
+    buf = malloc(sizeof(char) * letters);
+    if (buf == NULL) {
+        close(fd);
+        return 0;
+    }
+
+    t = read(fd, buf, letters);
+    if (t == -1) {
+        free(buf);
+        close(fd);
+        return 0;
+    }
+
+    w = write(STDOUT_FILENO, buf, t);
+
+    free(buf);
+    close(fd);
+    
+    if (w == -1 || (size_t)w != (size_t)t) {
+        return (0); 
+    }
+
+    return (w);
+}
+
